efc04e5733b15a7c8f28cfc470863d5141685dc8
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-25 12:51:00 -0800

    2/5 database fetch calls working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react';
+import {
+  fetchPinballAsync,
+  fetchPinballThen,
+  fetchGhibliAsync,
+  fetchGhibliThen,
+} from './services/promise-me';
+
 export default function App() {
+  useEffect(async () => {
+    const resp = await fetchGhibliThen();
+    console.log('resp', resp);
+  }, []);
+
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,35 +1,36 @@
 /**
- * TODO:
+ * TODO: Write 5 fetch requests to an API url using .then()
+ * TODO: Write 5 fetch requests using async/await
  */
 
-/**
- * TODO:
- */
+export const fetchPinballAsync = async () => {
+  const resp = await fetch('https://ponball.herokuapp.com/api/v1/machines');
+  const data = await resp.json();
 
-/**
- * TODO:
- */
+  return data;
+};
 
-/**
- * TODO:
- */
+export const fetchPinballThen = () => {
+  const data = fetch('https://ponball.herokuapp.com/api/v1/machines').then(
+    (data) => data.json()
+  );
 
-/**
- * TODO:
- */
+  return data;
+};
 
-/**
- * TODO:
- */
+export const fetchGhibliAsync = async () => {
+  const resp = await fetch(
+    'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
+  );
+  const data = await resp.json();
 
-/**
- * TODO:
- */
+  return data;
+};
 
-/**
- * TODO:
- */
+export const fetchGhibliThen = () => {
+  const data = fetch(
+    'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
+  ).then((data) => data.json());
 
-/**
- * TODO:
- */
+  return data;
+};
